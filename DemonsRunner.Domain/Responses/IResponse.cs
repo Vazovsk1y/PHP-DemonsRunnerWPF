23c96d3799d9ea4de31c9988b705fc8210fa107c
Vazovsk1y <anolrey.b@gmail.com>
2023-04-22 17:49:26 +0300

    -add response and his implementaion.

---
+++ DemonsRunner.Domain/Responses/IResponse.cs
@@ -0,0 +1,13 @@
+﻿using DemonsRunner.Enums;
+
+namespace DemonsRunner.Interfaces
+{
+    public interface IResponse<T> 
+    {
+        public string Description { get; set; }
+
+        public IEnumerable<T> Data { get; set; }
+        
+        public StatusCode OperationStatus { get; set; }
+    }
+}
---
+++ DemonsRunner/Implementations/Responses/Response.cs
@@ -0,0 +1,13 @@
+﻿using DemonsRunner.Enums;
+using DemonsRunner.Interfaces;
+using System.Collections.Generic;
+
+namespace DemonsRunner.Implementations.Responses
+{
+    public class Response<T> : IResponse<T>
+    {
+        public string Description { get; set; }
+        public IEnumerable<T> Data { get; set; }
+        public StatusCode OperationStatus { get; set; }
+    }
+}
