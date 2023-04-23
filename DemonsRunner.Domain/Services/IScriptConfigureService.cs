8cc2546aefb31f04238327116bed769011a0fae4
Vazovsk1y <anolrey.b@gmail.com>
2023-04-23 13:44:03 +0300

    -add script configure service.

---
+++ DemonsRunner.Domain/Services/IScriptConfigureService.cs
@@ -0,0 +1,10 @@
+﻿using DemonsRunner.Domain.Interfaces;
+using DemonsRunner.Domain.Models;
+
+namespace DemonsRunner.Domain.Services
+{
+    public interface IScriptConfigureService
+    {
+        public IResponse<PHPScript> ConfigureScripts(IEnumerable<PHPDemon> demons);
+    }
+}
---
+++ DemonsRunner/Implementations/Services/ScriptConfigureService.cs
@@ -0,0 +1,42 @@
+﻿using DemonsRunner.Domain.Enums;
+using DemonsRunner.Domain.Interfaces;
+using DemonsRunner.Domain.Models;
+using DemonsRunner.Domain.Services;
+using DemonsRunner.Implementations.Responses;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace DemonsRunner.Implementations.Services
+{
+    internal class ScriptConfigureService : IScriptConfigureService
+    {
+        public IResponse<PHPScript> ConfigureScripts(IEnumerable<PHPDemon> demons)
+        {
+            try
+            {
+                var configuredScripts = new List<PHPScript>();
+                foreach (var demon in demons)
+                {
+                    configuredScripts.Add(new PHPScript(demon));
+                }
+
+                return new Response<PHPScript>
+                {
+                    Description = "Scripts were successfully configurated!",
+                    OperationStatus = StatusCode.Success,
+                    Data = configuredScripts
+                }; 
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return new Response<PHPScript>
+                {
+                    Description = "Something go wrong!",
+                    OperationStatus = StatusCode.Fail
+                };
+            }
+        }
+    }
+}
