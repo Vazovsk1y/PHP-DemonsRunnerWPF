15d6c56774677ce7a3aafba4bc8a593c0c172175
Vazovsk1y <anolrey.b@gmail.com>
2023-04-23 13:43:37 +0300

    -add script model.

---
+++ DemonsRunner.Domain/Models/PHPScript.cs
@@ -0,0 +1,16 @@
+﻿namespace DemonsRunner.Domain.Models
+{
+    public class PHPScript
+    {
+        public string Name => $"{ExecutableFile.Name} script";
+
+        public string Command => $"demon .php {ExecutableFile.Name} start";
+
+        public PHPDemon ExecutableFile { get; }
+
+        public PHPScript(PHPDemon executableFile)
+        {
+            ExecutableFile = executableFile;
+        }
+    }
+}
