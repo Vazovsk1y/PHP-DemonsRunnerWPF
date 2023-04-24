ef779f503498b70dd05d788fa1d758ecbe327982
Vazovsk1y <anolrey.b@gmail.com>
2023-04-24 22:01:29 +0300

    -udpate script executor model.

--- DemonsRunner.Domain/Models/PHPScriptExecutor.cs
+++ DemonsRunner.Domain/Models/PHPScriptExecutor.cs
@@ -14,7 +14,7 @@ namespace DemonsRunner.Domain.Models
 
         public PHPScript ExecutableScript { get; }
 
-        public bool IsRunning { get; private set; } 
+        public bool IsRunning { get; private set; }
 
         public PHPScriptExecutor(PHPScript script, bool showExecutingWindow)
         {
@@ -36,36 +36,31 @@ namespace DemonsRunner.Domain.Models
             _executableConsole.OutputDataReceived += OnScriptOutputDataReceived;
         }
 
-        private void OnScriptOutputDataReceived(object sender, DataReceivedEventArgs e) => ScriptOutputMessageReceived?.Invoke(this, e);
+        private void OnScriptOutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                ScriptOutputMessageReceived?.Invoke(this, e);
+            }
+        }
 
         private void OnScriptExited(object? sender, EventArgs e) => ScriptExitedByUser?.Invoke(this, EventArgs.Empty);
 
         public void Start()
         {
-            try
-            {
-                _executableConsole.Start();
-                _executableConsole.StandardInput.WriteLine(ExecutableScript.Command);
-                _executableConsole.StandardInput.Flush();
-                IsRunning = true;
-            }
-            catch
-            {
-                throw;
-            }
+            if (_disposed) throw new ObjectDisposedException(nameof(PHPScriptExecutor));
+            _executableConsole.Start();
+            _executableConsole.StandardInput.WriteLine(ExecutableScript.Command);
+            _executableConsole.StandardInput.Flush();
+            _executableConsole.BeginOutputReadLine();
+            IsRunning = true;
         }
 
         public void Stop()
         {
-            try
-            {
-                _executableConsole.Kill();
-                IsRunning = false;
-            }
-            catch
-            {
-                throw;
-            }
+            if (_disposed) throw new ObjectDisposedException(nameof(PHPScriptExecutor));
+            _executableConsole.Kill();
+            IsRunning = false;
         }
 
         public void Dispose()
@@ -82,12 +77,12 @@ namespace DemonsRunner.Domain.Models
                 if (disposing)
                 {
                     // managed resources 
+                    _executableConsole.Exited -= OnScriptExited;
+                    _executableConsole.OutputDataReceived -= OnScriptOutputDataReceived;
+                    _executableConsole.Dispose();
                 }
 
                 // unmanaged resourses
-                _executableConsole.Exited -= OnScriptExited;
-                _executableConsole.OutputDataReceived -= OnScriptOutputDataReceived;
-                _executableConsole.Dispose();
                 _disposed = true;
             }
         }
