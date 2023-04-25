88753cead4ad611b6dd88dd835d5c8b2779d06e2
Vazovsk1y <anolrey.b@gmail.com>
2023-04-25 15:29:11 +0300

    -add new PHPScriptMessage model.

---
+++ DemonsRunner.Domain/Models/PHPScriptMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace DemonsRunner.Domain.Models
+{
+    public class PHPScriptMessage
+    {
+        public PHPScript Sender { get; }
+
+        public string Text { get; }
+
+        public PHPScriptMessage(PHPScript script, string messageText)
+        {
+            Sender = script;
+            Text = messageText;
+        }
+    }
+}
