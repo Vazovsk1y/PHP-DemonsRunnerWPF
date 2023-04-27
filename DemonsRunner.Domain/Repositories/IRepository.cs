c38bae3f8afccb717ee63c4d7d0ef7eb60f9c521
Vazovsk1y <anolrey.b@gmail.com>
2023-04-27 17:44:57 +0300

    -add repository to interact with json file.

---
+++ DemonsRunner.Domain/Repositories/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace DemonsRunner.Domain.Repositories
+{
+    public interface IRepository<T>
+    {
+        public IEnumerable<T> GetAll();
+
+        public void Save(IEnumerable<T> entities);
+    }
+}
