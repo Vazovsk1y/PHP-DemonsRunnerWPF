7d3e0c9d6ca4ecbf4f04715db1fbe89ced1a726e
Vazovsk1y <anolrey.b@gmail.com>
2023-04-28 16:28:29 +0300

    -add fileService and his implementation.

---
+++ DemonsRunner.Domain/Services/IFileService.cs
@@ -0,0 +1,13 @@
+﻿using DemonsRunner.Domain.Interfaces;
+using DemonsRunner.Domain.Models;
+using DemonsRunner.Domain.Responses;
+
+namespace DemonsRunner.Domain.Services
+{
+    public interface IFileService
+    {
+        public IDataResponse<PHPDemon> GetSaved();
+
+        public IBaseResponse Save(IEnumerable<PHPDemon> savedFiles);
+    }
+}
---
+++ DemonsRunner/Implementations/Services/FileService.cs
@@ -0,0 +1,75 @@
+﻿using DemonsRunner.DAL.Repositories;
+using DemonsRunner.Domain.Interfaces;
+using DemonsRunner.Domain.Models;
+using DemonsRunner.Domain.Repositories;
+using DemonsRunner.Domain.Responses;
+using DemonsRunner.Domain.Services;
+using DemonsRunner.Implementations.Responses;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace DemonsRunner.Implementations.Services
+{
+    internal class FileService : IFileService
+    {
+        private readonly IFileRepository<PHPDemon> _repository = new FileRepository();
+
+        public IDataResponse<PHPDemon> GetSaved()
+        {
+            try
+            {
+                var files = _repository.GetAllFiles().ToList();
+
+                if (files is null || files.Count == 0)
+                {
+                    return new DataResponse<PHPDemon>
+                    {
+                        Description = "Data json file not founded!",
+                        OperationStatus = Domain.Enums.StatusCode.Fail,
+                    };
+                }
+
+                return new DataResponse<PHPDemon>
+                {
+                    Description = "Files were succsessfully given to you!",
+                    OperationStatus = Domain.Enums.StatusCode.Success,
+                    Data = files
+                };
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return new DataResponse<PHPDemon>
+                {
+                    Description = "Something go wrong",
+                    OperationStatus = Domain.Enums.StatusCode.Fail,
+                };
+            }
+        }
+
+        public IBaseResponse Save(IEnumerable<PHPDemon> savedFiles)
+        {
+            try
+            {
+                _repository.SaveFiles(savedFiles);
+
+                return new BaseResponse
+                {
+                    Description = "Files were succsessfully saved",
+                    OperationStatus = Domain.Enums.StatusCode.Success,
+                };
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return new DataResponse<PHPDemon>
+                {
+                    Description = "Something go wrong",
+                    OperationStatus = Domain.Enums.StatusCode.Fail,
+                };
+            }
+        }
+    }
+}
