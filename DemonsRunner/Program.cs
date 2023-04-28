1e38681345827146cbb3f3aa2ba8c5e83841eeff
Vazovsk1y <anolrey.b@gmail.com>
2023-04-28 16:54:14 +0300

    -change startup object.

---
+++ DemonsRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DemonsRunner
+{
+    internal class Program
+    {
+        [STAThread]
+        internal static void Main(string[] args)
+        {
+            App app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
