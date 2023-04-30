0a0f38a83629b966f700326e3e544ac7d8372bd0
Vazovsk1y <anolrey.b@gmail.com>
2023-04-30 15:46:19 +0300

    -update executor viewModel, replace it again on vm.

---
+++ DemonsRunner/ViewModels/PHPScriptExecutorViewModel.cs
@@ -0,0 +1,92 @@
+﻿using DemonsRunner.Domain.Models;
+using DemonsRunner.ViewModels.Base;
+using System.Diagnostics;
+using System;
+using System.Collections.ObjectModel;
+
+namespace DemonsRunner.ViewModels
+{
+    internal class PHPScriptExecutorViewModel : BaseViewModel, IDisposable
+    {
+        #region --Events--
+
+        public event EventHandler? ScriptExited;
+
+        #endregion
+
+        #region --Fields--
+
+        private bool _disposed = false;
+
+        #endregion
+
+        #region --Properties--
+
+        public ObservableCollection<string> OutputMessages { get; } = new ObservableCollection<string>();
+
+        public PHPScriptExecutor ScriptExecutor { get; }
+
+        #endregion
+
+        #region --Constructors--
+
+        public PHPScriptExecutorViewModel(PHPScriptExecutor scriptExecutor)
+        {
+            ScriptExecutor = scriptExecutor;
+            ScriptExecutor.ScriptOutputMessageReceived += OnScriptOutputMessageReceived;
+            ScriptExecutor.ScriptExitedByUser += OnScriptExited;
+        }
+
+        #endregion
+
+        #region --Commands--
+
+
+
+        #endregion
+
+        #region --Methods--
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private async void OnScriptExited(object? sender, EventArgs e)
+        {
+            if (sender is PHPScriptExecutor scriptExecutor)
+            {
+                await App.Current.Dispatcher.InvokeAsync(() =>
+                {
+                    ScriptExited?.Invoke(this, e);
+                });
+            }
+        }
+
+        private async void OnScriptOutputMessageReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (sender is PHPScriptExecutor scriptExecutor)
+            {
+                await App.Current.Dispatcher.InvokeAsync(() => OutputMessages.Add(e.Data! + DateTime.Now));
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    ScriptExecutor.ScriptOutputMessageReceived -= OnScriptOutputMessageReceived;
+                    ScriptExecutor.ScriptExitedByUser -= OnScriptExited;
+                    ScriptExecutor.Dispose();
+                    OutputMessages.Clear();
+                }
+                _disposed = true;
+            }
+        }
+
+        #endregion
+    }
+}
