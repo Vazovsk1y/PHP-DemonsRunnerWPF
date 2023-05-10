ec2f3f20c66e2b308ad33c1bd252863c34551d15
Vazovsk1y <anolrey.b@gmail.com>
2023-05-10 13:58:27 +0300

    -add response factory.

---
+++ DemonsRunner.BuisnessLayer/Services/Interfaces/IResponseFactory.cs
@@ -0,0 +1,14 @@
+﻿using DemonsRunner.Domain.Enums;
+using DemonsRunner.Domain.Responses.Intefaces;
+
+namespace DemonsRunner.BuisnessLayer.Services.Interfaces
+{
+    public interface IResponseFactory
+    {
+        public IResponse CreateResponse(StatusCode statusCode, string description);
+
+        public IDataResponse<T> CreateDataResponse<T>(StatusCode statusCode, string description, T? data = null) where T : class;
+
+        public IDataResponse<IEnumerable<T>> CreateDataResponse<T>(StatusCode statusCode, string description, IEnumerable<T> data);
+    }
+}
---
+++ DemonsRunner.BuisnessLayer/Services/ResponseFactory.cs
@@ -0,0 +1,39 @@
+﻿using DemonsRunner.BuisnessLayer.Services.Interfaces;
+using DemonsRunner.Domain.Enums;
+using DemonsRunner.Domain.Responses;
+using DemonsRunner.Domain.Responses.Intefaces;
+
+namespace DemonsRunner.BuisnessLayer.Services
+{
+    public class ResponseFactory : IResponseFactory
+    {
+        public IResponse CreateResponse(StatusCode statusCode, string description)
+        {
+            return new Response
+            {
+                Description = description,
+                OperationStatus = statusCode,
+            };
+        }
+
+        public IDataResponse<T> CreateDataResponse<T>(StatusCode statusCode, string description, T? data = null) where T : class
+        {
+            return new DataResponse<T>
+            {
+                OperationStatus = statusCode,
+                Description = description,
+                Data = data
+            };
+        }
+
+        public IDataResponse<IEnumerable<T>> CreateDataResponse<T>(StatusCode statusCode, string description, IEnumerable<T> data)
+        {
+            return new DataResponse<IEnumerable<T>>
+            {
+                OperationStatus = statusCode,
+                Description = description,
+                Data = data
+            };
+        }
+    }
+}
