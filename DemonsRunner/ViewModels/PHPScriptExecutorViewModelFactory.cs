ebf8ecfcf2f749be5462ff2b60cbb2c9df1f98d5
Vazovsk1y <anolrey.b@gmail.com>
2023-05-11 19:23:14 +0300

    -add script executor view model factory.

---
+++ DemonsRunner/ViewModels/PHPScriptExecutorViewModelFactory.cs
@@ -0,0 +1,26 @@
+﻿using DemonsRunner.BuisnessLayer.Services.Interfaces;
+using DemonsRunner.Domain.Models;
+using DemonsRunner.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemonsRunner.ViewModels
+{
+    internal class PHPScriptExecutorViewModelFactory : IScriptExecutorViewModelFactory
+    {
+        private readonly IDataBus _dataBus;
+
+        public PHPScriptExecutorViewModelFactory(IDataBus dataBus)
+        {
+            _dataBus = dataBus;
+        }
+
+        public IScriptExecutorViewModel CreateViewModel(PHPScriptExecutor scriptExecutor)
+        {
+            return new PHPScriptExecutorViewModel(scriptExecutor, _dataBus);
+        }
+    }
+}
