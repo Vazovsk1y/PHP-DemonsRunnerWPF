e5ce76265f28df5e5ad3d1a45e5b29aa084662e8
Vazovsk1y <anolrey.b@gmail.com>
2023-06-03 14:19:22 +0300

    -fixed a bug when opening a release downloaded from github on a user's PC.

--- DemonsRunner/Program.cs
+++ DemonsRunner/Program.cs
@@ -13,49 +13,32 @@ namespace DemonsRunner
         [STAThread]
         public static void Main(string[] args)
         {
-            MessageBox.Show("IN MAIN");
-            try
-            {
-                App app = new();
-                app.Run();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-           
+            App app = new();
+            app.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) => Host
             .CreateDefaultBuilder(args)
             .UseSerilog((host, loggingConfiguration) =>
             {
-                try
+                string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                string appName = AppDomain.CurrentDomain.FriendlyName;
+
+                string logFileDirectoryPath = Path.Combine(appDataPath, appName);
+                if (!Directory.Exists(logFileDirectoryPath))
                 {
-                    string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-                    string appName = AppDomain.CurrentDomain.FriendlyName;
+                    Directory.CreateDirectory(logFileDirectoryPath);
+                }
 
-                    string logFileDirectoryPath = Path.Combine(appDataPath, appName);
-                    if (!Directory.Exists(logFileDirectoryPath))
-                    {
-                        Directory.CreateDirectory(logFileDirectoryPath);
-                    }
+                string logFileName = "log.txt";
+                string logFileFullPath = Path.Combine(logFileDirectoryPath, logFileName);
 
-                    string logFileName = "log.txt";
-                    string logFileFullPath = Path.Combine(logFileDirectoryPath, logFileName);
-
-                    loggingConfiguration.MinimumLevel.Information();
+                loggingConfiguration.MinimumLevel.Information();
 #if DEBUG
-                    loggingConfiguration.WriteTo.Debug();
+                loggingConfiguration.WriteTo.Debug();
 #else
                 loggingConfiguration.WriteTo.File(logFileFullPath, rollingInterval: RollingInterval.Day);
 #endif
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Execption throwned in use serilog \n{ex.Message}\n{ex.StackTrace}");
-                }
-               
             })
             .UseContentRoot(App.CurrentDirectory)
             .ConfigureServices(App.ConfigureServices)
